10a32c8c403098c7f108b9ae938caf8c947cbfd5
RyomaQ <ryoma1@hotmail.fr>
2023-11-21 14:42:46 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#2D2D2D",
+        },
+      },
+    },
+  
+  plugins: [],
+  darkMode: "class",
+}
